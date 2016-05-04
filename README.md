f761597d8608212649bac5a28e1c56a51a87e130
amitgandhinz <amit.gandhi@rackspace.com>
2016-05-03 21:58:14 -0400

    Added initial parsing logic

---
+++ README.md
@@ -0,0 +1,31 @@
+SEO Report
+==========
+
+Scan your website for On Page SEO Optimization issues based on 
+the SEO guidelines from Google's SEO Starter Guide:
+        
+http://static.googleusercontent.com/media/www.google.com/en//webmasters/docs/search-engine-optimization-starter-guide.pdf
+
+
+Setup
+-----
+
+PIP:
+```
+pip install -r requirements.txt
+```
+
+Docker:
+
+```
+docker-compose build
+docker-compose up -d
+```
+
+
+Usage
+-----
+
+```
+./seo_report.py http://www.domain.com
+```
---
+++ website.py
@@ -0,0 +1,64 @@
+import webpage
+
+from bs4 import BeautifulSoup as Soup
+
+import json
+import requests
+
+
+class Spider(object):
+
+    pages_to_crawl = []
+    pages_crawled = []
+
+    titles = {}
+    descriptions = {}
+    
+    report = {
+            "pages": []
+        }
+
+    def __init__(self, site, sitemap=None):
+        # look for the sitemap on this page
+        if sitemap is not None:
+            # add all locations in the sitemap to the pages_to_crawl table
+            locations = self._parse_sitemap(sitemap)
+            self.pages_to_crawl.extend(locations)
+        else:
+            self.pages_to_crawl.append(site)
+        
+    def _parse_sitemap(self, url):
+        '''
+        Parse the Sitemap for Locations
+        '''
+        output = []
+        
+        resp = requests.get(url)
+        
+        # we didn't get a valid response, bail
+        if not resp.ok:
+            return []
+        
+        soup = Soup(resp.content, "html.parser")
+        urls = soup.findAll('url')
+        
+        # get just the locations
+        if len(url) > 0:
+            for u in urls:
+                loc = u.find('loc').string
+                output.append(loc)
+            
+        return output
+
+    def crawl(self):
+        # iterate over pages to crawl
+        for page in self.pages_to_crawl:
+            html = webpage.Webpage(page, self.titles, self.descriptions)
+            
+            page_report = html.report()
+            self.report['pages'].append(page_report)
+            
+            # mark the page as crawled
+            self.pages_crawled.append(page.strip().lower())
+
+        print json.dumps(self.report, indent=4, separators=(',', ': '))
